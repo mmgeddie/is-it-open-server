d4d058822c535d0a5dfb4d2d92583212cc27b383
Matt Geddie <matt@geddie.org>
2015-03-10 06:59:01 -0500

    Updated Haraka plugin to contact 'client' endpoint, removed express

--- haraka/plugins/data.isitopen.js
+++ haraka/plugins/data.isitopen.js
@@ -1,4 +1,4 @@
-// Simple string signatures
+var http = require('http');
 
 exports.hook_data = function (next, connection) {
     // enable mail body parsing
@@ -8,5 +8,36 @@ exports.hook_data = function (next, connection) {
 
 exports.hook_data_post = function (next, connection) {  
 	this.loginfo('Body: ' + connection.transaction.body);  
+
+	var establishment = {
+	  	name: "Matt Geddie",
+		// time: new Date ("3/10/2015, 3:52 AM."+" CDT")
+		time: new Date()
+	};
+
+	var establishmentString = JSON.stringify(establishment);
+
+	var headers = {
+	  	'Content-Type': 'application/json',
+	  	'Content-Length': establishmentString.length
+	};
+
+	var options = {
+	  	host: 'is-it-open.herokuapp.com',
+	  	port: 80,
+	  	path: '/establishments',
+	  	method: 'POST',
+	  	headers: headers
+	};
+
+	var req = http.request(options);
+
+	req.on('error', function(e) {
+		connection.logerror(e);
+	});
+
+	req.write(establishmentString);
+	req.end();
+
     return next();
 }
--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-var express = require('express');
 var path    = require('path');
-
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use('/scripts', express.static(path.join(__dirname, './public/scripts')));
-
-app.all("/*", function(req, res, next) {
-    res.sendFile("index.html", { root: path.join(__dirname, './public') });
-});
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
-
 
 process.env.HARAKA = path.join(path.resolve('.'), 'haraka');
 var haraka  = require('./node_modules/Haraka/haraka.js');
